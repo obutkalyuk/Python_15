10345acab2c5aae1bb43e017bb16492990e2e0d0
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-03-04 21:02:05 +0100

    Task3
    tests for contacts

---
+++ contact.py
@@ -0,0 +1,16 @@
+
+class Contact:
+    def __init__(self, firstName, lastName, address, homePhone,mobilePhone, workPhone, email1, email2, email3):
+        self.firstName = firstName
+
+        self.lastName = lastName
+        self.address = address
+        self.homePhone = homePhone
+        self.mobilePhone = mobilePhone
+        self.workPhone = workPhone
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+
+
+
