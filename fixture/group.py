64cdff6e8e8cef827eb67b65b506ed185bf424b8
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-03-25 23:13:42 +0100

    Test Modify group

--- fixture/group.py
+++ fixture/group.py
@@ -7,17 +7,24 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
-        self.app.type_text("group_name", group.name)
-        self.app.type_text("group_header", group.header)
-        self.app.type_text("group_footer", group.footer)
+        self.set_fields(group)
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
-    def delete_first_group(self):
+    def delete_first(self):
         wd = self.app.wd
-        self.open_group_page();
+        self.open_group_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+
+    def modify_first(self, edition):
+        wd = self.app.wd
+        self.open_group_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        self.set_fields(edition)
+        wd.find_element_by_name("update").click()
         self.return_to_group_page()
 
     def return_to_group_page(self):
@@ -27,3 +34,8 @@ class GroupHelper:
     def open_group_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
+
+    def set_fields(self, group):
+        self.app.type_text("group_name", group.name)
+        self.app.type_text("group_header", group.header)
+        self.app.type_text("group_footer", group.footer)
--- test/Test_Group_Delete.py
+++ test/Test_Group_Delete.py
@@ -2,5 +2,5 @@
 
 def test_delete_firstgroup(app):
     app.session.login( user="admin", password="secret")
-    app.group.delete_first_group()
+    app.group.delete_first()
     app.session.logout()
