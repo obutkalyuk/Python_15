766fcfa5e7f0d163d5821fe1367a71aa347ab19b
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-03-25 14:49:43 +0100

    add pytest

--- Tests.py
+++ Tests.py
@@ -1,53 +1,42 @@
 # -*- coding: utf-8 -*-
-import unittest
 import pytest
 from application import Application
 from contact import Contact
 from group import Group
 
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizser(fixture.destroy)
+    return fixture
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+def test_add_group(app):
+    app.login( user="admin", password="secret")
+    app.create_group( Group(name="Group1", header="header", footer="footer1"))
+    app.return_to_group_page()
+    app.logout()
 
-class test_and_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+def test_add_empty_group(app):
+    app.login(user="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.return_to_group_page()
+    app.logout()
 
-    def test_add_group(self):
-        self.app.login( user="admin", password="secret")
-        self.app.create_group( Group(name="Group1", header="header", footer="footer1"))
-        self.app.return_to_group_page()
-        self.app.logout()
+def test_add_contacts(app):
+    app.login(user="admin", password="secret")
+    app.create_contact(Contact(firstName="James", lastName="Bond", address="London", homePhone="123",
+                                    mobilePhone="456", workPhone="789", email1="email1@com.com",
+                                    email2="email2@com.com", email3="email3@com.com"))
+    app.return_to_home_page()
+    app.logout()
 
-    def test_add_empty_group(self):
-        self.app.login(user="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.return_to_group_page()
-        self.app.logout()
+def test_add_empty_contact(app):
+    app.login( user="admin", password="secret")
+    app.create_contact(Contact(firstName="", lastName="", address="", homePhone="", mobilePhone="",
+                                    workPhone="", email1="", email2="", email3=""))
+    app.return_to_home_page()
+    app.logout()
 
-    def test_add_contacts(self):
-        self.app.login(user="admin", password="secret")
-        self.app.create_contact(Contact(firstName="James", lastName="Bond", address="London", homePhone="123",
-                                        mobilePhone="456", workPhone="789", email1="email1@com.com",
-                                        email2="email2@com.com", email3="email3@com.com"))
-        self.app.return_to_home_page()
-        self.app.logout()
-
-    def test_add_empty_contact(self):
-        self.app.login( user="admin", password="secret")
-        self.app.create_contact(Contact(firstName="", lastName="", address="", homePhone="", mobilePhone="",
-                                        workPhone="", email1="", email2="", email3=""))
-        self.app.return_to_home_page()
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
-if __name__ == '__main__':
-    unittest.main()
 
 
 
