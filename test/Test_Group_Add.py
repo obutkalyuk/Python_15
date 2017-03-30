03355098bfa6f667fe8a45e5006153c0f82f9609
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-03-31 00:19:48 +0200

    add verifications to group tests

--- test/Test_Group_Add.py
+++ test/Test_Group_Add.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
+
 def test_add_group(app):
+    new_group = Group(name="Group1", header="header", footer="footer1")
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="Group1", header="header", footer="footer1"))
+    app.group.create(new_group)
     new_groups = app.group.get_group_list()
     assert len(old_groups)+ 1 == len(new_groups)
+    old_groups.append(new_group)
+
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
 def test_add_empty_group(app):
+    new_group = Group(name="", header="", footer="")
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    app.group.create(new_group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups)+ 1 == len(new_groups)
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(new_group)
+
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/Test_Group_Modify.py
+++ test/Test_Group_Modify.py
@@ -2,13 +2,16 @@
 from model.group import Group
 
 def test_modify_first_group(app):
+    edition = Group(name="Group_first")
 
     if app.group.count() == 0:
         app.group.create(Group(name="Test"))
     old_groups = app.group.get_group_list()
-    edition = Group(name="Group_first")
+    edition.id = old_groups[0].id
     app.group.modify_first(edition)
     new_groups = app.group.get_group_list()
     assert len(old_groups)  == len(new_groups)
+    old_groups[0] = edition
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
