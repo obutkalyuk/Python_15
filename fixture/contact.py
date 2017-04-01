a1f59957a9d69d9967cc994e98521f36758da888
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-04-01 18:31:19 +0200

    add cache for groups and contacts

--- fixture/contact.py
+++ fixture/contact.py
@@ -4,11 +4,14 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
+    contact_cache = None
+
     def create_contact(self, contact):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
         self.set_fields(contact)
         wd.find_element_by_name("submit").click()
+        self.contact_cache = None
 
     def set_fields(self, contact):
         self.app.type_text("firstname", contact.firstName)
@@ -26,6 +29,7 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_css_selector("[value='Delete']").click()
         wd.switch_to_alert().accept()
+        self.contact_cache = None
 
     def find_contact(self, contact):
         wd = self.app.wd
@@ -43,6 +47,7 @@ class ContactHelper:
         wd = self.app.wd
         self.set_fields(edition)
         wd.find_element_by_name("update").click()
+        self.contact_cache = None
 
     def modify_first_contact(self,edition):
         wd = self.app.wd
@@ -51,21 +56,23 @@ class ContactHelper:
         editLink.click()
         self.set_fields(edition)
         wd.find_element_by_name("update").click()
+        self.contact_cache = None
 
     def count(self):
         wd = self.app.wd
         return len(wd.find_elements_by_name("selected[]"))
 
     def get_contact_list(self):
-        wd = self.app.wd
-        contacts = []
-        for row in  wd.find_elements_by_css_selector("tr[name='entry']"):
-            id = row.find_element_by_name("selected[]").get_attribute("id")
-            last_name = row.find_element_by_xpath(".//td[2]").text
-            fisrt_name = row.find_element_by_xpath(".//td[3]").text
-            address = row.find_element_by_xpath(".//td[4]").text
-            email = row.find_element_by_xpath(".//td[5]").text
-            phone = row.find_element_by_xpath(".//td[6]").text
-            contact = Contact(id=id, firstName=fisrt_name, lastName=last_name, address=address)
-            contacts.append(contact)
-        return contacts
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.contact_cache = []
+            for row in wd.find_elements_by_css_selector("tr[name='entry']"):
+                id = row.find_element_by_name("selected[]").get_attribute("id")
+                last_name = row.find_element_by_xpath(".//td[2]").text
+                fisrt_name = row.find_element_by_xpath(".//td[3]").text
+                address = row.find_element_by_xpath(".//td[4]").text
+                email = row.find_element_by_xpath(".//td[5]").text
+                phone = row.find_element_by_xpath(".//td[6]").text
+                contact = Contact(id=id, firstName=fisrt_name, lastName=last_name, address=address)
+                self.contact_cache.append(contact)
+        return list(self.contact_cache)
--- fixture/group.py
+++ fixture/group.py
@@ -4,6 +4,8 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
+    group_cache = None
+
     def create(self, group):
         wd = self.app.wd
         self.open_group_page()
@@ -11,6 +13,7 @@ class GroupHelper:
         self.set_fields(group)
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def delete_first(self):
         wd = self.app.wd
@@ -18,6 +21,7 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def modify_first(self, edition):
         wd = self.app.wd
@@ -27,6 +31,7 @@ class GroupHelper:
         self.set_fields(edition)
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def count(self):
         wd = self.app.wd
@@ -46,13 +51,15 @@ class GroupHelper:
         self.app.type_text("group_name", group.name)
         self.app.type_text("group_header", group.header)
 
+
     def get_group_list(self):
-        groups = []
-        wd = self.app.wd
-        self.open_group_page()
-        for element in wd.find_elements_by_css_selector("span.group"):
-           text = element.text
-           id = element.find_element_by_name("selected[]").get_attribute("value")
-           groups.append(Group(name = text,id = id))
-        return groups
+        if self.group_cache is None:
+            self.group_cache = []
+            wd = self.app.wd
+            self.open_group_page()
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
 
