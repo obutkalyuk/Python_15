60bacd6cd0f99f8e4d3103d528c729fb1db074df
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-04-18 17:28:46 +0200

    groups - load parameters from data file

--- data/add_group.py
+++
@@ -1,9 +0,0 @@
-from model.group import Group
-from fixture.string_helper import random_string
-
-constant = [Group(name="name1", header="header1", footer="footer1"),
-            Group(name="name2", header="header2", footer="footer2")]
-
-testdata = [Group(name="", header="", footer="")] + \
-          [Group(name= random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-          for i in range(5)]
--- generator/group.py
+++ generator/group.py
@@ -11,7 +11,7 @@ except  getopt.GetoptError as err:
     getopt.usage()
     sys.exit(2)
 
-n = 3
+n = 5
 f = "data/group.json"
 
 for o, a in opts:
