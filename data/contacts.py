17bd17cb8d1d3ed41f314a5c533e14d473e3906f
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-04-19 09:47:34 +0200

    contact - update Test_contact_add for using generated data

---
+++ data/contacts.py
@@ -0,0 +1,10 @@
+from model.contact import Contact
+
+
+testdata = [Contact(firstName="FisrtName_1", lastName="LastName_1", address="Address_1",
+                    homePhone="111111", mobilePhone="111112", workPhone="111113",
+                    email1="email1@mail_1.com", email2="email2@mail_1.com", email3="email3@mail_1.com"),
+            Contact(firstName="FisrtName_2", lastName="LastName_2", address="Address_2",
+                    homePhone="222221", mobilePhone="222222", workPhone="222223",
+                    email1="email1@mail_2.com", email2="email2@mail_2.com", email3="email3@mail_2.com")
+            ]
--- test/Test_Contact_Add.py
+++ test/Test_Contact_Add.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
-
 from model.contact import Contact
-import pytest
-from fixture.string_helper import random_string
-from fixture.string_helper import random_set
-from fixture.string_helper import random_digits
 
-testdata = [Contact(firstName="", lastName="", address="", homePhone="", mobilePhone="",
-                               workPhone="", email1="", email2="", email3="")] + \
-          [Contact(firstName= random_string("First", 7),
-                   lastName=random_string("Last", 7),
-                   address = random_string("address", 20),
-                   homePhone = random_digits (10),
-                   mobilePhone = random_digits (10),
-                   workPhone = random_digits (10),
-                   email1= "%s@%s.%s" % ("email1", random_set(5), random_set(3)),
-                   email2="%s@%s.%s" % ("email2", random_set(5), random_set(3)),
-                   email3="%s@%s.%s" % ("email3", random_set(5), random_set(3)))
-          for i in range(5)]
-
-@pytest.mark.parametrize("contact", testdata, ids= [repr(x) for x in testdata])
-def test_add_contacts(app, contact):
+def test_add_contacts(app, json_contacts):
+    contact = json_contacts
     new_contact = contact
     old_contacts = app.contact.get_contact_list()
     app.contact.create(new_contact)
@@ -32,8 +14,5 @@ def test_add_contacts(app, contact):
 
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max), "Test message"
 
-# def test_add_empty_contact(app):
-#     app.contact.create(Contact(firstName="", lastName="", address="", homePhone="", mobilePhone="",
-#                                workPhone="", email1="", email2="", email3=""))
-#     app.return_to_home_page()
+
 
