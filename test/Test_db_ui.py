c9e75b9617e5df964f471e52d4695a5963b3e0ec
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-05-03 16:25:43 +0200

    get time of execute

--- test/Test_db_ui.py
+++ test/Test_db_ui.py
@@ -1,13 +1,17 @@
 from model.group import Group
 from model.contact import Contact
+from timeit import timeit
+
 
 def test_group_db_list(app, db):
+    print(timeit(lambda:app.group.get_group_list(), number=1))
     ui_list = app.group.get_group_list()
     def clean(group):
         return Group(id=group.id, name = group.name.strip())
 
     db_list = map(clean, db.get_group_list())
-    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    print(timeit(lambda:map(clean, db.get_group_list()), number=1000))
+    assert False #sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
 
 
 def test_contact_db_list(app, db):
