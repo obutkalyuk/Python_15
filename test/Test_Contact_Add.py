314977490982b1adc10b019bf00331d815d4aafb
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-05-04 16:14:25 +0200

    modify add contact with db

--- test/Test_Contact_Add.py
+++ test/Test_Contact_Add.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 
-def test_add_contacts(app, json_contacts):
+def test_add_contacts(app, db, json_contacts):
     contact = json_contacts
     new_contact = contact
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     app.contact.create(new_contact)
     app.return_to_home_page()
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
 
-    assert len(old_contacts) + 1 == app.contact.count()
     old_contacts.append(new_contact)
 
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/Test_Group_Delete.py
+++ test/Test_Group_Delete.py
@@ -2,15 +2,15 @@
 from model.group import Group
 from random import randrange
 
-def test_delete_random_group(app):
+def test_delete_random_group(app, db):
     if app.group.count() == 0:
         app.group.create(Group(name="Test"))
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     index = randrange(len(old_groups))
     app.group.delete_by_index(index)
 
     assert len(old_groups) - 1 == app.group.count()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups[index:index+1]=[]
     assert old_groups==new_groups
 
