3486091e915c833cb9d51e35a6189bddd121cdb6
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-05-07 10:51:18 +0200

    delete group - using db

--- fixture/group.py
+++ fixture/group.py
@@ -21,6 +21,11 @@ class GroupHelper:
         xpath = "//span[%s]/input[@name='selected[]']" % str(index+1)
         wd.find_element_by_xpath(xpath).click()
 
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        css = "input[value='%s']" % str(id)
+        wd.find_element_by_css_selector(css).click()
+
     def delete_first(self):
         self.delete_by_index(0)
 
@@ -28,6 +33,14 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         self.select_group_by_index(index)
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+        self.group_cache = None
+
+    def delete_by_id(self, id):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_group_by_id(id)
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
         self.group_cache = None
--- test/Test_Group_Delete.py
+++ test/Test_Group_Delete.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from random import randrange
+import random
 
 def test_delete_random_group(app, db):
-    if app.group.count() == 0:
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name="Test"))
     old_groups = db.get_group_list()
-    index = randrange(len(old_groups))
-    app.group.delete_by_index(index)
+    group = random.choice(old_groups)
+    app.group.delete_by_id(group.id)
 
     assert len(old_groups) - 1 == app.group.count()
     new_groups = db.get_group_list()
-    old_groups[index:index+1]=[]
+    old_groups.remove(group)
     assert old_groups==new_groups
 
