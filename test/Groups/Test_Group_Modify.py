c6bd9a1b16d4541112c0bb09a2e8f8f9339c4852
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-05-12 18:17:43 +0200

    clear tests

--- test/Groups/Test_Group_Modify.py
+++ test/Groups/Test_Group_Modify.py
@@ -24,16 +24,4 @@ def test_modify_random_group(app, db, check_ui):
 
 
 
-def test_group_list(app):
-    edition = Group(name="Group_first")
-    ui_list = app.group.get_group_list()
-    ui_list2 = [Group(name="test2", header="header", footer='footer')]
 
-    assert sorted(ui_list, key=Group.id_or_max) == sorted(ui_list2, key=Group.id_or_max)
-
-def test_group_list2():
-   ui_list = [Group(name="1"), Group(name='2'), Group(name='3')]
-   ui_list2 = list(ui_list)
-   ui_list2[0] = Group(name='5')
-
-   assert sorted(ui_list, key=Group.id_or_max) == sorted(ui_list2, key=Group.id_or_max)
