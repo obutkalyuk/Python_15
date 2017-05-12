b6729056e55698282e65e3fb4821153cb21cc085
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-05-12 23:02:22 +0200

    add alllure to test

--- test/Groups/Test_Group_Add.py
+++ test/Groups/Test_Group_Add.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
 
 
 def test_add_group(app, db, check_ui, json_groups):
     group = json_groups
     new_group = group
-    old_groups = db.get_group_list()
-    app.group.create(new_group)
-    new_groups = db.get_group_list()
-    old_groups.append(new_group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with pytest.allure.step("Given a group list"):
+        old_groups = db.get_group_list()
+    with pytest.allure.step('When I add a group %s to the list' % new_group):
+        app.group.create(new_group)
+    with pytest.allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(new_group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
 
